78feb2068d37f0149123552d2f8e5c2b90088885
Muammar El Khatib <muammarelkhatib@gmail.com>
2016-03-25 23:59:12 +0100

    Getting rid of multithreading to reconnect.

--- mkchromecast/streaming.py
+++ mkchromecast/streaming.py
@@ -20,6 +20,9 @@ import multiprocessing
 import time
 import sys
 from cast import *
+import psutil
+import os
+import signal
 
 """
 These functions are used to get up the streaming server.
@@ -29,32 +32,38 @@ To call them:
     name()
 """
 
-def stream():
-    def launch_server():
-        webcast = ['./bin/node', './nodejs/node_modules/webcast-osx-audio/bin/webcast.js']
-        p = subprocess.Popen(webcast)
-        while p.poll() is None:
-            try:
-                time.sleep(0.5)
-            except KeyboardInterrupt:
-                print ("Ctrl-c was requested")
-                sys.exit(0)
-        else:
-            print ('Reconnecting streaming...')
+def streaming():
+    webcast = ['./bin/node', './nodejs/node_modules/webcast-osx-audio/bin/webcast.js']
+    p = subprocess.Popen(webcast)
+    while p.poll() is None:
+        try:
+            time.sleep(0.5)
+        except KeyboardInterrupt:
+            print ("Ctrl-c was requested")
+            sys.exit(0)
+    else:
+        print ('Reconnecting streaming...')
+        relaunch(stream,recasting,kill)
 
-            relaunch(stream,recasting)
-
-    #thread = threading.Thread(target=launch_server)
-    #thread.daemon = True
-    #thread.start()
-    thread = multiprocessing.Process(target=launch_server)
-    thread.daemon = False
-    thread.start()
     return
 
-def relaunch(func,func1):
-    func()
+class multi_proc(object):
+    def __init__(self):
+        self.proc = multiprocessing.Process(target=streaming)
+        self.proc.daemon = False
+
+    def start(self):
+        self.proc.start()
+
+def kill():
+    pid=getpid()
+    os.kill(pid, signal.SIGTERM)
+
+def relaunch(func1,func2,func3):
     func1()
+    func2()
+    func3()
+
     return
 
 def recasting():
@@ -63,3 +72,7 @@ def recasting():
     start.get_cc()
     start.play_cast()
     return
+
+def stream():
+    st = multi_proc()
+    st.start()
