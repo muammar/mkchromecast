fd4d65a02fdbdb7a5c3e179f0f8ca72a975f596e
Muammar El Khatib <muammarelkhatib@gmail.com>
2016-03-27 11:45:01 +0200

    - Updated requirements.txt - Now we check that the computer is connected to the network before starting.

--- mkchromecast.py
+++ mkchromecast.py
@@ -8,11 +8,18 @@ from mkchromecast.cast import *
 from mkchromecast.terminate import *
 import atexit
 
+
+cc = casting()
+
+if cc.ip == '127.0.0.1' or None:        # We verify the local IP.
+    print ('Your computer is not connected to any network')
+    terminate()
+
 print('Switching to soundflower')
+
 inputdev()
 outputdev()
 stream()
-cc = casting()
 cc.initialize_cast()
 cc.get_cc()
 cc.play_cast()
