a789b20c8381241d46392f8c8c2e52fb01540cb6
Muammar El Khatib <muammarelkhatib@gmail.com>
2016-04-01 18:39:29 +0200

    Some more actions to the menubar :). Learning QT5!.

--- systrayqt.py
+++ systrayqt.py
@@ -1,30 +1,47 @@
 #!/usr/bin/env python
 
+from mkchromecast.audiodevices import *
+from mkchromecast.cast import *
 from PyQt5 import QtCore, QtGui, QtWidgets
 import signal
 
-self.cc = casting()
 
-def main():
+cc = casting()
+class menubar(object):
+    def __init__(self):
+        signal.signal(signal.SIGINT, signal.SIG_DFL)
 
-    signal.signal(signal.SIGINT, signal.SIG_DFL)
+        app = QtWidgets.QApplication([])
 
-    app = QtWidgets.QApplication([])
+        icon = QtGui.QIcon('images/google.ico')
+        tray = QtWidgets.QSystemTrayIcon(icon)
+        menu = QtWidgets.QMenu()
+        self.SearchAction = menu.addAction("Search for Google cast devices")
+        self.SearchAction.triggered.connect(self.search_cast)
 
-    icon = QtGui.QIcon('images/google.ico')
-    tray = QtWidgets.QSystemTrayIcon(icon)
-    menu = QtWidgets.QMenu()
-    SearchAction = menu.addAction("Search for Google cast devices")
-    StopAction = menu.addAction("Stop casting")
-    resetAction = menu.addAction("Reset audio")
-    preferencesAction = menu.addAction("Preferences")
-    exitAction = menu.addAction("Exit")
-    exitAction.triggered.connect(app.quit)
-    tray.setContextMenu(menu)
-    tray.show()
+        self.StopCastAction = menu.addAction("Stop casting")
+        self.StopCastAction.triggered.connect(self.stop_cast)
 
+        self.ResetAudioAction = menu.addAction("Reset audio")
+        self.ResetAudioAction.triggered.connect(self.reset_audio)
 
-    app.exec_()
+        exitAction = menu.addAction("Exit")
+        exitAction.triggered.connect(app.quit)
+        tray.setContextMenu(menu)
+        tray.show()
+        app.exec_()
+
+    def search_cast(self):
+        args.select_cc = True
+        cc.initialize_cast()
+        print (cc.availablecc)
+
+    def stop_cast(self):
+        cc.stop_cast()
+
+    def reset_audio(self):
+        inputint()
+        outputint()
 
 if __name__ == '__main__':
-    main()
+    menubar()
