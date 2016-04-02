fb3f902f6a270fe92e5a3e0c4f7fd38023d93e94
Muammar El Khatib <muammarelkhatib@gmail.com>
2016-04-03 00:24:23 +0200

    Systray almost ready!!!

--- systrayqt.py
+++ systrayqt.py
@@ -13,36 +13,54 @@ class menubar(object):
     def __init__(self):
         signal.signal(signal.SIGINT, signal.SIG_DFL)
 
-        app = QtWidgets.QApplication([])
+        self.app = QtWidgets.QApplication([])
 
         icon = QtGui.QIcon('images/google.ico')
         tray = QtWidgets.QSystemTrayIcon(icon)
         self.menu = QtWidgets.QMenu()
+        self.search_menu()
+        self.stop_menu()
+        self.separator_menu()
+        self.populating_menu()
+        self.separator_menu()
+        self.resetaudio_menu()
+        self.about_menu()
+        self.exit_menu()
+
+        tray.setContextMenu(self.menu)
+        tray.show()
+        self.app.exec_()
+
+    def search_menu(self):
         self.SearchAction = self.menu.addAction("Search for Google cast devices")
         self.SearchAction.triggered.connect(self.search_cast)
 
+    def stop_menu(self):
         self.StopCastAction = self.menu.addAction("Stop casting")
         self.StopCastAction.triggered.connect(self.stop_cast)
 
+    def separator_menu(self):
         self.menu.addSeparator()
-        self.StopCastAction = self.menu.addAction("Available Google cast devices")
-        if self.StopCastAction.triggered.connect == True:
+
+    def populating_menu(self):
+        self.PopulationCastAction = self.menu.addAction("List of Google Cast devices:")
+        if self.SearchAction.triggered.connect == True:
             self.cast_list()
 
-        self.menu.addSeparator()
+    def resetaudio_menu(self):
         self.ResetAudioAction = self.menu.addAction("Reset audio")
         self.ResetAudioAction.triggered.connect(self.reset_audio)
 
-        self.ResetAudioAction = self.menu.addAction("About")
+    def about_menu(self):
+        self.AboutAction = self.menu.addAction("About")
 
+    def exit_menu(self):
         exitAction = self.menu.addAction("Exit")
-        exitAction.triggered.connect(app.quit)
-        tray.setContextMenu(self.menu)
-        tray.show()
-        app.exec_()
+        exitAction.triggered.connect(self.app.quit)
 
-
-
+    """
+    These are methods for interacting with the mkchromecast objects
+    """
 
     def search_cast(self):
         args.select_cc = True
@@ -53,10 +71,19 @@ class menubar(object):
         if len(cc.availablecc) == 0:
             print ('No devices found!')
         else:
+            self.menu.clear()
+            self.search_menu()
+            self.stop_menu()
+            self.separator_menu()
+            self.PopulationCastAction = self.menu.addAction("List of Google Cast devices:")
             for menuentry in cc.availablecc:
                 print ('Lo hizo!')
                 print menuentry[0]
-                self.entry = self.menu.addAction(str(menuentry[0]))
+                self.entry = self.menu.addAction(str(menuentry[1]))
+            self.separator_menu()
+            self.resetaudio_menu()
+            self.about_menu()
+            self.exit_menu()
 
     def stop_cast(self):
         cc.stop_cast()
