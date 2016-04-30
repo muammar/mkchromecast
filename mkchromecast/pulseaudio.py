608a228c661a13a6c5e50bd6c8f277ca7da69797
Muammar El Khatib <muammarelkhatib@gmail.com>
2016-04-30 14:42:04 +0200

    subprocess for creating, and deleting sinks.

--- mkchromecast/pulseaudio.py
+++ mkchromecast/pulseaudio.py
@@ -2,11 +2,18 @@
 
 # This file is part of mkchromecast.
 
+import mkchromecast.__init__
+import subprocess
+
 def create_sink():
-    pacmd load-module module-null-sink sink_name=MySink
-    pacmd update-sink-proplist MySink device.description=MySink
+
+    create_sink = ['pacmd', 'load-module', 'module-null-sink', 'sink_name=', name]
+    rename_sink = ['pacmd', 'update-sink-proplist', name, 'device.description=', name]
+    create_p = subprocess.Popen(create_sink)
+    rename_p = subprocess.Popen(rename_sink)
     return
 
 def remove_sink():
-    pacmd unload-module module-null-sink
+    remove_sink = ['pacmd', 'unload-module', 'module-null-sink']
+    rename_p = subprocess.Popen(remove_sink)
     return
