3140f8728159d77bb270b526ff97fb5050d70ee4
Muammar El Khatib <muammarelkhatib@gmail.com>
2016-05-03 22:47:53 +0200

    File to add colors to prints.

---
+++ mkchromecast/colors.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+# This file is part of mkchromecast.
+
+import sys
+
+BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = range(8)
+
+#following from Python cookbook, #475186
+def has_colours(stream):
+    if not hasattr(stream, "isatty"):
+        return False
+    if not stream.isatty():
+        return False # auto color only on TTYs
+    try:
+        import curses
+        curses.setupterm()
+        return curses.tigetnum("colors") > 2
+    except:
+        # guess false in case of error
+        return False
+has_colours = has_colours(sys.stdout)
+
+
+def printout(text, colour=WHITE):
+        if has_colours:
+                seq = "\x1b[1;%dm" % (30+colour) + text + "\x1b[0m"
+                sys.stdout.write(seq)
+        else:
+                sys.stdout.write(text)
