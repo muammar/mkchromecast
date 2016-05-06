2a20333767d5816790cd6fce6380afdc7547119a
Muammar El Khatib <muammarelkhatib@gmail.com>
2016-05-06 15:48:54 +0200

    Adding debian branch to create a debian package.

---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(mkchromecast):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="mkchromecast" command="/usr/bin/mkchromecast"
---
+++ debian/mkchromecast.default.ex
@@ -0,0 +1,10 @@
+# Defaults for mkchromecast initscript
+# sourced by /etc/init.d/mkchromecast
+# installed at /etc/default/mkchromecast by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
