2bd1789a30d5cb2579649cc29a97ab8cfc08f892
Muammar El Khatib <muammarelkhatib@gmail.com>
2016-05-14 16:41:09 +0200

    Everything is in place.

--- mkchromecast/preferences.py
+++ mkchromecast/preferences.py
@@ -33,9 +33,9 @@ class Example(QWidget):
         Backend
         """
         self.backend = QLabel('Select Backend', self)
-        self.backend.move(20, 54)
+        self.backend.move(20, 24)
         self.qcbackend = QComboBox(self)
-        self.qcbackend.move(180, 50)
+        self.qcbackend.move(180, 20)
         self.qcbackend.setMinimumContentsLength(7)
         self.qcbackend.addItem("node")
         self.qcbackend.addItem("ffmpeg")
@@ -47,9 +47,9 @@ class Example(QWidget):
         """
         self.bitrate = QLabel('Select Bitrate (kbit/s)', self)
         #self.bitrate.setAlignment(QtCore.Qt.AlignLeft)
-        self.bitrate.move(20, 86)
+        self.bitrate.move(20, 56)
         self.qcbitrate = QComboBox(self)
-        self.qcbitrate.move(180, 84)
+        self.qcbitrate.move(180, 54)
         self.qcbitrate.setMinimumContentsLength(7)
         self.qcbitrate.addItem("128")
         self.qcbitrate.addItem("160")
@@ -61,12 +61,27 @@ class Example(QWidget):
         self.qcbitrate.activated[str].connect(self.onActivated)
 
         """
+        Sample rate
+        """
+        self.samplerate = QLabel('Sample rate (Hz)', self)
+        #self.samplerate.setAlignment(QtCore.Qt.AlignLeft)
+        self.samplerate.move(20, 88)
+        self.qcsamplerate = QComboBox(self)
+        self.qcsamplerate.move(180, 88)
+        self.qcsamplerate.setMinimumContentsLength(7)
+        self.qcsamplerate.addItem("48000")
+        self.qcsamplerate.addItem("44100")
+        self.qcsamplerate.addItem("32000")
+        self.qcsamplerate.addItem("22050")
+        self.qcsamplerate.activated[str].connect(self.onActivated)
+
+        """
         Notifications
         """
         self.notifications = QLabel('Notifications', self)
-        self.notifications.move(20, 118)
+        self.notifications.move(20, 120)
         self.qcnotifications = QComboBox(self)
-        self.qcnotifications.move(180, 118)
+        self.qcnotifications.move(180, 120)
         self.qcnotifications.setMinimumContentsLength(7)
         self.qcnotifications.addItem("Enabled")
         self.qcnotifications.addItem("Disabled")
