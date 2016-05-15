81efde74db7992812806c4059fbb8d5242c6e995
Muammar El Khatib <muammarelkhatib@gmail.com>
2016-05-15 19:35:47 +0200

    Keep it simple.

--- mkchromecast/config.py
+++ mkchromecast/config.py
@@ -6,37 +6,11 @@ import ConfigParser
 
 config = ConfigParser.RawConfigParser()
 
-config.add_section('backend')
-config.set('backend', 'an_int', '15')
-config.set('backend', 'a_bool', 'true')
-config.set('backend', 'a_float', '3.1415')
-config.set('backend', 'baz', 'fun')
-config.set('backend', 'bar', 'Python')
-config.set('backend', 'foo', '%(bar)s is %(baz)s!')
-
-config.add_section('codec')
-config.set('codec', 'an_int', '15')
-config.set('codec', 'a_bool', 'true')
-config.set('codec', 'a_float', '3.1415')
-config.set('codec', 'baz', 'fun')
-config.set('codec', 'bar', 'Python')
-config.set('codec', 'foo', '%(bar)s is %(baz)s!')
-
-config.add_section('bitrate')
-config.set('bitrate', 'an_int', '15')
-config.set('bitrate', 'a_bool', 'true')
-config.set('bitrate', 'a_float', '3.1415')
-config.set('bitrate', 'baz', 'fun')
-config.set('bitrate', 'bar', 'Python')
-config.set('bitrate', 'foo', '%(bar)s is %(baz)s!')
-
-config.add_section('samplerate')
-config.set('samplerate', 'an_int', '15')
-config.set('samplerate', 'a_bool', 'true')
-config.set('samplerate', 'a_float', '3.1415')
-config.set('samplerate', 'baz', 'fun')
-config.set('samplerate', 'bar', 'Python')
-config.set('samplerate', 'foo', '%(bar)s is %(baz)s!')
+config.add_section('settings')
+config.set('settings', 'backend', '15')
+config.set('settings', 'codec', 'true')
+config.set('settings', 'bitrate', '3.1415')
+config.set('settings', 'samplerate', 'fun')
 
 # Writing our configuration file to 'example.cfg'
 with open('mkchromecast.cfg', 'wb') as configfile:
@@ -59,4 +33,4 @@ def ConfigSectionMap(section):
             dict1[option] = None
     return dict1
 
-print (ConfigSectionMap("backend")['baz'])
+print (ConfigSectionMap("settings")['bitrate'])
