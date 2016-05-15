006bea14ddb8397c820e1c7aa3b88d641bdd36f4
Muammar El Khatib <muammarelkhatib@gmail.com>
2016-05-15 16:04:07 +0200

    Improvements config.py.

--- mkchromecast/config.py
+++ mkchromecast/config.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 # This file is part of mkchromecast.
+
 import ConfigParser
 
 config = ConfigParser.RawConfigParser()
@@ -40,3 +41,22 @@ config.set('samplerate', 'foo', '%(bar)s is %(baz)s!')
 # Writing our configuration file to 'example.cfg'
 with open('mkchromecast.cfg', 'wb') as configfile:
     config.write(configfile)
+
+"""
+The function below helps to map the options inside each section. Taken from:
+https://wiki.python.org/moin/ConfigParserExamples
+"""
+def ConfigSectionMap(section):
+    dict1 = {}
+    options = config.options(section)
+    for option in options:
+        try:
+            dict1[option] = config.get(section, option)
+            if dict1[option] == -1:
+                DebugPrint("skip: %s" % option)
+        except:
+            print("exception on %s!" % option)
+            dict1[option] = None
+    return dict1
+
+print (ConfigSectionMap("backend")['baz'])
