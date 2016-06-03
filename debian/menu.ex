438870b876c617e013bde73eb90d5efdc33fdc84
Muammar El Khatib <muammarelkhatib@gmail.com>
2016-06-03 22:53:44 +0200

    Filtering debian/gbp.conf unneeded files.

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(mkchromecast):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="mkchromecast" command="/usr/bin/mkchromecast"
--- debian/mkchromecast.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the mkchromecast package
-#
-0 4	* * *	root	[ -x /usr/bin/mkchromecast_maintenance ] && /usr/bin/mkchromecast_maintenance
