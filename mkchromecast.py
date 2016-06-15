4d9640d940e0000bb8ebcd9e22dd19d8033af1a7
Muammar El Khatib <muammarelkhatib@gmail.com>
2016-06-15 17:44:55 +0200

    The time that sound muted is now reduced when launching from command line for Mac. For Linux users, they have to select the sink in pavucontrol still. This could change later.

--- mkchromecast.py
+++ mkchromecast.py
@@ -27,12 +27,7 @@ if args.tray == False:
         terminate()
 
     if args.youtube == None:
-        if platform == 'Darwin':
-            print('Switching to soundflower...')
-            inputdev()
-            outputdev()
-            print(colors.success('[Done]'))
-        else:
+        if platform == 'Linux':
             print('Creating pulseaudio sink...')
             print(colors.warning('Open pavucontrol and select the mkchromecast sink.'))
             create_sink()
@@ -55,9 +50,19 @@ if args.tray == False:
         cc.sel_cc()
         cc.inp_cc()
         cc.get_cc()
+        if platform == 'Darwin':
+            print('Switching to soundflower...')
+            inputdev()
+            outputdev()
+            print(colors.success('[Done]'))
         cc.play_cast()
     else:
         cc.get_cc()
+        if platform == 'Darwin':
+            print('Switching to soundflower...')
+            inputdev()
+            outputdev()
+            print(colors.success('[Done]'))
         cc.play_cast()
 
     def terminateapp():
