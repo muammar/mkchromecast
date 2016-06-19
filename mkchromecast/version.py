1149d75827d8cb71a6598b97d0d3f4b07b252f51
Muammar El Khatib <muammarelkhatib@gmail.com>
2016-06-19 17:25:13 +0200

    Added a function to verify updates.

--- mkchromecast/version.py
+++ mkchromecast/version.py
@@ -3,3 +3,18 @@
 # This file is part of mkchromecast.
 
 __version__ = '0.2.8'
+
+def updater():
+    import requests
+    url = 'https://api.github.com/repos/muammar/mkchromecast/releases/latest'
+    response = requests.get(url).text.split(',')
+
+    for e in response:
+        if 'tag_name' in e:
+            version = e.strip('"tag_name":')
+
+    if version > __version__:
+        print ('Version ' + version + ' is available to download')
+    else:
+        print ('You are up to date')
+        return
