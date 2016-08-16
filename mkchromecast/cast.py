4fdd77e07f76e36c188979837b14a5f3a600813d
Muammar El Khatib <muammarelkhatib@gmail.com>
2016-08-16 09:43:42 +0200

    get_chromecast doesn't like the quotes. Thanks to @phraim.

--- mkchromecast/cast.py
+++ mkchromecast/cast.py
@@ -210,7 +210,7 @@ class casting(object):
         if self.debug == True:
             print('def get_cc(self):')
         try:
-            self.cast = pychromecast.get_chromecast('friendly_name="'+self.castto+'"')
+            self.cast = pychromecast.get_chromecast(friendly_name=self.castto)
             # Wait for cast device to be ready
             self.cast.wait()
             print(' ')
