0cd6bb94c18602da7e887e3d8d21c7e1a6684355
Muammar El Khatib <muammarelkhatib@gmail.com>
2017-11-26 08:53:57 -0500

    Error with "width not divisible by 2 (853x480)" is fixed in this commit. Closes issue #119.

--- mkchromecast/resolution.py
+++ mkchromecast/resolution.py
@@ -3,14 +3,14 @@
 # This file is part of mkchromecast.
 
 resolutions = {
-               '480p': ('scale-853:-1', '480x854'),
-               '720p': ('scale=1280:-1', '1280x720'),
-               '1366x768': ('scale=1366x768', '1366x768'),
-               '1080p': ('scale=1920x1080', '1920x1080'),
-               '2k': ('scale=2048x1148', '2048x1148'),
-               'uhd': ('scale=3840x2160', '3840x2160'),
-               '4k': ('scale=4096:-1', '4096x2160')
-               }
+        '480p': ('scale-854:-1', '854x480:'),
+        '720p': ('scale=1280:-1', '1280x720'),
+        '1366x768': ('scale=1366x768', '1366x768'),
+        '1080p': ('scale=1920x1080', '1920x1080'),
+        '2k': ('scale=2048x1148', '2048x1148'),
+        'uhd': ('scale=3840x2160', '3840x2160'),
+        '4k': ('scale=4096:-1', '4096x2160')
+        }
 
 def resolution(res, screencast):
         res = resolutions[res.lower()]
