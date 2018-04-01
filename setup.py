2db9c17d1db95dcdd63278fcfc51d1d4902f5dd6
Alec Leamas <leamas.alec@gmail.com>
2018-04-01 09:51:57 +0200

    setup.py: Support linux installations
    Add code to use setup.py also on Linux platforms. This is basically
    to move installation code from the Debian packaging to the upstream
    package. It makes the package a standard python package with correct
    locations and will make maintenance as well as packaging on other distros
    simpler.

--- setup.py
+++ setup.py
@@ -3,21 +3,61 @@
 # This file is part of mkchromecast.
 
 """
-py2app build script for mkchromecast
+Linux/MacOS build script for mkchromecast
 
-Usage:
+
+MacOS usage:
     python3 setup.py py2app
-    cp -R /usr/local/Cellar/qt5/5.6.0/plugins dist/mkchromecast.app/Contents/PlugIns
+    cp -R /usr/local/Cellar/qt5/5.6.0/plugins \
+        dist/mkchromecast.app/Contents/PlugIns
     macdeployqt dist/mkchromecast.app
 
-You need to install using pip3 the following:
+On MacOS you need to install using pip3 the following:
 
     bs4
     google
+
+On Linux, this is a standard distutils script supporting
+
+   python3 setup.py build
+   python3 setup.py install
+
+etc., with standard setup.py options. Note that there are some non-python
+dependencies not listed here; see README.md.
 """
+import os
+import platform
+
+from glob import glob
 from setuptools import setup
 
-version=open('mkchromecast/version.py').readlines()[-1].split()[-1].strip("\"'")
+ROOT = os.path.dirname(__file__)
+ROOT = ROOT if ROOT else '.'
+
+VERSION = \
+    open('mkchromecast/version.py').readlines()[-1].split()[-1].strip("\"'")
+
+LINUX_DATA = [
+    ('share/mkchromecast/nodejs', glob('nodejs/*')),
+    ('share/mkchromecast/images', glob('images/google*.png')),
+    ('share/applications/', ['mkchromecast.desktop']),
+    ('share/man/man1', ['mkchromecast.1']),
+]
+LINUX_REQUIRES = [
+    'flask',
+    'mutagen',
+    'netifaces',
+    'psutil',
+    'PyChromecast',
+    'PyQt5',
+    'requests'
+]
+LINUX_CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: End Users/Desktop',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+]
 
 APP = ['start_tray.py']
 APP_NAME = 'Mkchromecast'
@@ -57,20 +97,40 @@ OPTIONS = {
         'CFBundleDisplayName': APP_NAME,
         'CFBundleGetInfoString': 'Cast macOS audio to your Google cast devices and Sonos speakers',
         'CFBundleIdentifier': 'com.mkchromecast.osx',
-        'CFBundleVersion': version,
-        'CFBundleShortVersionString': version,
+        'CFBundleVersion': VERSION,
+        'CFBundleShortVersionString': VERSION,
         'NSHumanReadableCopyright': u'Copyright (c) 2017, Muammar El Khatib, All Rights Reserved',
         'LSPrefersPPC': True,
         'LSUIElement': True
     }
 }
 
-setup(
-    name=APP_NAME,
-    app=APP,
-    data_files=DATA_FILES,
-    package='Mkchromecast',
-    platforms=['i386', 'x86_64'],
-    options={'py2app': OPTIONS},
-    setup_requires=['py2app'],
-)
+if platform.system() == 'Darwin':
+    setup(
+        name=APP_NAME,
+        app=APP,
+        data_files=DATA_FILES,
+        package='Mkchromecast',
+        platforms=['i386', 'x86_64'],
+        options={'py2app': OPTIONS},
+        setup_requires=['py2app'],
+    )
+
+elif platform.system() == 'Linux':
+    setup(
+        name='mkchromecast',
+        version=VERSION,
+        description='Cast Linux audio or video to Google Cast devices',
+        long_description=open(ROOT + '/README.md').read(),
+        include_package_data=True,
+        license='MIT',
+        url='http://mkchromecast.com/',
+        author='Muammar El Khatib',
+        author_email='http://muammar.me/',
+        keywords=['chromecast'],
+        packages=['mkchromecast'],
+        scripts=['bin/mkchromecast'],
+        classifiers=LINUX_CLASSIFIERS,
+        data_files=LINUX_DATA,
+        requires=LINUX_REQUIRES
+    )
