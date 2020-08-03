2874366707183306d28ed619003777973b18f3cb
Daniel Perelman <perelman@cs.washington.edu>
2020-08-03 12:08:48 -0700

    Set sink description in load-module command.

--- mkchromecast/pulseaudio.py
+++ mkchromecast/pulseaudio.py
@@ -7,22 +7,11 @@ import time
 def create_sink():
     sink_name = "Mkchromecast"
 
-    create_sink = ["pactl", "load-module", "module-null-sink", "sink_name=" + sink_name]
-
-    rename_sink = [
-        "pacmd",
-        "update-sink-proplist",
-        sink_name,
-        "device.description=" + sink_name,
-    ]
+    create_sink = ["pactl", "load-module", "module-null-sink", "sink_name=" + sink_name, "sink_properties=device.description=" + sink_name]
 
     cs = subprocess.Popen(create_sink, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     csoutput, cserror = cs.communicate()
 
-    time.sleep(1)
-
-    rs = subprocess.Popen(rename_sink, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    rsoutput, rserror = rs.communicate()
     return
 
 
