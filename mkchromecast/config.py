f52388acaa7d2a40016694f931edb7c530c67399
Omari Stephens <xsdg@xsdg.org>
2024-02-08 05:10:44 +0000

    Drops the legacy config manager and adds missing features to Config class.

--- mkchromecast/config.py
+++ mkchromecast/config.py
@@ -1,30 +1,29 @@
 # This file is part of mkchromecast.
 
 import configparser
-import functools
-import getpass
 import os
 import pathlib
 from typing import Optional
 
-import mkchromecast
+# NOTE: Can't import mkchromecast because that would create a circular dependency.
 
 # Section name.
 SETTINGS = "settings"
 
 # Field names.
+BACKEND = "backend"
 CODEC = "codec"
 BITRATE = "bitrate"
 SAMPLERATE = "samplerate"
 NOTIFICATIONS = "notifications"
 COLORS = "colors"
-SEARCH_AT_LAUNCH = "searchatlaunch"
-ALSA_DEVICE = "alsadevice"
+SEARCH_AT_LAUNCH = "search_at_launch"
+ALSA_DEVICE = "alsa_device"
 
 
-def _default_config_path(mkcc: mkchromecast.Mkchromecast) -> pathlib.Path:
+def _default_config_path(platform: str) -> pathlib.Path:
     config_dir: pathlib.PurePath
-    if mkcc.platform == "Darwin":
+    if platform == "Darwin":
         config_dir = pathlib.PosixPath(
             "~/Library/Application Support/mkchromecast")
     else:  # Linux
@@ -40,150 +39,147 @@ def _default_config_path(mkcc: mkchromecast.Mkchromecast) -> pathlib.Path:
 
 
 class Config:
-    def __init__(self, config_path: Optional[os.PathLike] = None):
-        self._mkcc = mkchromecast.Mkchromecast()
+    def __init__(self,
+                 platform: str,
+                 config_path: Optional[os.PathLike] = None,
+                 read_only: bool = False,
+                 debug: bool = False):
+        self._debug = debug
+        self._platform = platform
+        self._read_only = read_only
 
-        if not config_path:
-            config_path = _default_config_path(self._mkcc)
+        if config_path:
+            self._config_path = config_path
+        else:
+            self._config_path = _default_config_path(self._platform)
 
-        self.config = configparser.ConfigParser()
+        self._config = configparser.ConfigParser()
+
+        self._default_conf = {
+            CODEC: "mp3",
+            BITRATE: 192,
+            SAMPLERATE: 44100,
+            NOTIFICATIONS: False,
+            COLORS: "black",
+            SEARCH_AT_LAUNCH: False,
+            ALSA_DEVICE: None,
+        }
+
+        if self._platform == "Darwin":
+            self._default_conf[BACKEND] = "node"
+        else:
+            self._default_conf[BACKEND] = "parec"
+
+    def __enter__(self):
+        """Parses config file and returns self"""
+        self._config.read(self._config_path)
+        self._update_any_missing_values()
+
+        return self
+
+    def __exit__(self, *exc):
+        self._maybe_write_config()
+
+    def _maybe_write_config(self) -> None:
+        """Writes the config to config_file unless read-only mode was used."""
+        if self._read_only:
+            return
+
+        with open(self._config_path, "wt") as config_file:
+            self._config.write(config_file)
+
+    def _update_any_missing_values(self) -> None:
+        """Sets any missing values to their defaults."""
+        expected_keys = self._default_conf.keys()
+        missing_keys: list[str] = []
+        for key in expected_keys:
+            if not self._config.has_option(SETTINGS, key):
+                missing_keys.append(key)
+                if self._debug:
+                    print(f":::config::: Setting missing key {key} to default "
+                          "value.")
+
+                # We use setattr to avoid bypassing any validation code that
+                # might exist.
+                setattr(self, key, self._default_conf[key])
+
+        if missing_keys:
+            if self._read_only:
+                print(":::config::: Missing keys _not_ being saved for "
+                      "read-only config")
+            else:
+                if self._debug:
+                    print(":::config::: Re-writing config to add missing keys: "
+                          f"{missing_keys}")
+
+                self._maybe_write_config()
 
     # TODO(xsdg): Refactor this to avoid code duplication.  Sadly,
     # functools.partialmethod doesn't work with properties.
     @property
+    def backend(self) -> str:
+        return self._config.get(SETTINGS, BACKEND)
+
+    @backend.setter
+    def backend(self, value: str) -> None:
+        self._config.set(SETTINGS, BACKEND, value)
+
+    @property
     def codec(self) -> str:
-        return self.config.get(SETTINGS, CODEC)
+        return self._config.get(SETTINGS, CODEC)
 
     @codec.setter
     def codec(self, value: str) -> None:
-        self.config.set(SETTINGS, CODEC, value)
+        self._config.set(SETTINGS, CODEC, value)
 
     @property
     def bitrate(self) -> int:
-        return self.config.getint(SETTINGS, BITRATE)
+        return self._config.getint(SETTINGS, BITRATE)
 
     @bitrate.setter
     def bitrate(self, value: int) -> None:
-        self.config.set(SETTINGS, BITRATE, str(value))
+        self._config.set(SETTINGS, BITRATE, str(value))
 
     @property
     def samplerate(self) -> int:
-        return self.config.getint(SETTINGS, SAMPLERATE)
+        return self._config.getint(SETTINGS, SAMPLERATE)
 
     @samplerate.setter
     def samplerate(self, value: int) -> None:
-        self.config.set(SETTINGS, SAMPLERATE, str(value))
+        self._config.set(SETTINGS, SAMPLERATE, str(value))
 
     @property
     def notifications(self) -> bool:
-        return self.config.getboolean(SETTINGS, NOTIFICATIONS)
+        return self._config.getboolean(SETTINGS, NOTIFICATIONS)
 
     @notifications.setter
     def notifications(self, value: bool) -> None:
-        self.config.set(SETTINGS, NOTIFICATIONS, str(value))
+        self._config.set(SETTINGS, NOTIFICATIONS, str(value))
 
     @property
     def colors(self) -> str:
-        return self.config.get(SETTINGS, COLORS)
+        return self._config.get(SETTINGS, COLORS)
 
     @colors.setter
     def colors(self, value: str) -> None:
-        self.config.set(SETTINGS, COLORS, value)
+        self._config.set(SETTINGS, COLORS, value)
 
     @property
     def search_at_launch(self) -> bool:
-        return self.config.getboolean(SETTINGS, SEARCH_AT_LAUNCH)
+        return self._config.getboolean(SETTINGS, SEARCH_AT_LAUNCH)
 
     @search_at_launch.setter
     def search_at_launch(self, value: bool) -> None:
-        self.config.set(SETTINGS, SEARCH_AT_LAUNCH, str(value))
+        self._config.set(SETTINGS, SEARCH_AT_LAUNCH, str(value))
 
     @property
-    def alsa_device(self) -> str:
-        return self.config.get(SETTINGS, ALSA_DEVICE)
+    def alsa_device(self) -> Optional[str]:
+        stored_value = self._config.get(SETTINGS, ALSA_DEVICE)
+        if stored_value == "None":
+            return None
+
+        return stored_value
 
     @alsa_device.setter
-    def alsa_device(self, value: str) -> None:
-        self.config.set(SETTINGS, ALSA_DEVICE, value)
-
-
-class config_manager:
-    def __init__(self):
-        self._mkcc = mkchromecast.Mkchromecast()
-
-        self.config = configparser.ConfigParser()
-
-        self.config.add_section("settings")
-        self.default_conf = {
-            "codec": "mp3",
-            "bitrate": "192",
-            "samplerate": "44100",
-            "notifications": "disabled",
-            "colors": "black",
-            "searchatlaunch": "disabled",
-            "alsadevice": None,
-        }
-
-        if self._mkcc.platform == "Darwin":
-            self.default_conf["backend"] = "node"
-        else:
-            self.default_conf["backend"] = "parec"
-
-        # Writing our configuration file
-
-        self.config_path = _default_config_path(self._mkcc)
-        self.config_dir = self.config_path.parent
-
-    def config_defaults(self):
-        """
-        Verify that the directory exists.
-        """
-        self.config_dir.mkdir(parents=True, exist_ok=True)
-
-        """
-        Creation of new configuration file with defaults.
-        """
-        if not self.config_path.exists():
-            self.write_defaults()
-
-    def write_defaults(self):
-        if self._mkcc.platform == "Darwin":
-            self.config.set("settings", "backend", "node")
-        else:
-            self.config.set("settings", "backend", "parec")
-
-        self.config.set("settings", "codec", "mp3")
-        self.config.set("settings", "bitrate", "192")
-        self.config.set("settings", "samplerate", "44100")
-        self.config.set("settings", "notifications", "disabled")
-        self.config.set("settings", "colors", "black")
-        self.config.set("settings", "searchatlaunch", "disabled")
-        self.config.set("settings", "alsadevice", None)
-
-        with open(self.config_path, "w") as config_file:
-            self.config.write(config_file)
-
-    def chk_config(self):
-        self.config.read(self.config_path)
-
-        """
-        We check that configuration file is complete, otherwise the settings
-        are filled from self.defaultconf dictionary.
-        """
-        expected_keys = self.default_conf.keys()
-        missing_keys: list[str] = []
-        for key in expected_keys:
-            if not self.config.has_option("settings", key):
-                missing_keys.append(key)
-                if self._mkcc.debug:
-                    print(f":::config::: Setting missing key {key} to default "
-                          "value.")
-                self.config.set("settings", key, self.defaultconf[key])
-
-        if missing_keys:
-            if self._mkcc.debug:
-                print(":::config::: Re-writing config to add missing keys: "
-                      f"{missing_keys}")
-
-            with open(self.config_path, "w") as config_file:
-                self.config.write(config_file)
+    def alsa_device(self, value: Optional[str]) -> None:
+        self._config.set(SETTINGS, ALSA_DEVICE, str(value))
--- tests/test_config.py
+++ tests/test_config.py
@@ -6,23 +6,36 @@ import pathlib
 import unittest
 from unittest import mock
 
-import mkchromecast
 from mkchromecast import config
 
 class ClampBitrateTests(unittest.TestCase):
     def setUp(self):
-        mock_mkcc = self.enterContext(mock.patch.object(
-            mkchromecast, "Mkchromecast", autospec=True))
         self.enterContext(mock.patch.object(os, "environ", autospec=True))
         self.enterContext(mock.patch.object(configparser, "ConfigParser", autospec=True))
+        self.enterContext(mock.patch("builtins.open", autospec=True))
 
     def testInstantiateNewConfig(self):
+        mock_parser = configparser.ConfigParser()
         config_path = pathlib.PurePath("/fake_dir/fake_path.txt")
-        conf = config.Config(config_path)
+        conf = config.Config(platform="Linux", config_path=config_path)
+        with conf:
+            mock_parser.read.assert_called_once_with(config_path)
+
+        mock_parser.write.assert_called_once()
+
+    def testInstantiateNewReadOnlyConfig(self):
+        mock_parser = configparser.ConfigParser()
+        config_path = pathlib.PurePath("/fake_dir/fake_path.txt")
+        conf = config.Config(
+            platform="Darwin", config_path=config_path, read_only=True)
+        with conf:
+            mock_parser.read.assert_called_once_with(config_path)
+
+        mock_parser.write.assert_not_called()
 
     def testPropertyGetters(self):
         config_path = pathlib.PurePath("/fake_dir/fake_path.txt")
-        conf = config.Config(config_path)
+        conf = config.Config("Linux", config_path)
         mock_parser = configparser.ConfigParser.return_value
         props = {
             "codec": str,
@@ -36,65 +49,30 @@ class ClampBitrateTests(unittest.TestCase):
         for prop_name, prop_type in props.items():
             with self.subTest(prop=prop_name):
                 mock_parser.reset_mock()
-                field_name = prop_name.replace("_", "")
                 _ = getattr(conf, prop_name)
 
                 if prop_type == str:
                     mock_parser.get.assert_called_once_with(
-                        config.SETTINGS, field_name)
+                        config.SETTINGS, prop_name)
                     mock_parser.getboolean.assert_not_called()
                     mock_parser.getint.assert_not_called()
                 elif prop_type == int:
                     mock_parser.get.assert_not_called()
                     mock_parser.getint.assert_called_once_with(
-                        config.SETTINGS, field_name)
+                        config.SETTINGS, prop_name)
                     mock_parser.getboolean.assert_not_called()
                 elif prop_type == bool:
                     mock_parser.get.assert_not_called()
                     mock_parser.getint.assert_not_called()
                     mock_parser.getboolean.assert_called_once_with(
-                        config.SETTINGS, field_name)
-
-    def testPropertyGetters(self):
-        config_path = pathlib.PurePath("/fake_dir/fake_path.txt")
-        conf = config.Config(config_path)
-        mock_parser = configparser.ConfigParser.return_value
-        props = {
-            "codec": str,
-            "bitrate": int,
-            "samplerate": int,
-            "notifications": bool,
-            "colors": str,
-            "search_at_launch": bool,
-            "alsa_device": str}
-
-        for prop_name, prop_type in props.items():
-            with self.subTest(prop=prop_name):
-                mock_parser.reset_mock()
-                field_name = prop_name.replace("_", "")
-                _ = getattr(conf, prop_name)
-
-                if prop_type == str:
-                    mock_parser.get.assert_called_once_with(
-                        config.SETTINGS, field_name)
-                    mock_parser.getboolean.assert_not_called()
-                    mock_parser.getint.assert_not_called()
-                elif prop_type == int:
-                    mock_parser.get.assert_not_called()
-                    mock_parser.getint.assert_called_once_with(
-                        config.SETTINGS, field_name)
-                    mock_parser.getboolean.assert_not_called()
-                elif prop_type == bool:
-                    mock_parser.get.assert_not_called()
-                    mock_parser.getint.assert_not_called()
-                    mock_parser.getboolean.assert_called_once_with(
-                        config.SETTINGS, field_name)
+                        config.SETTINGS, prop_name)
 
     def testPropertySetters(self):
         config_path = pathlib.PurePath("/fake_dir/fake_path.txt")
-        conf = config.Config(config_path)
+        conf = config.Config("Linux", config_path)
         mock_parser = configparser.ConfigParser.return_value
         props = {
+            "backend": str,
             "codec": str,
             "bitrate": int,
             "samplerate": int,
@@ -106,9 +84,24 @@ class ClampBitrateTests(unittest.TestCase):
         for prop_name, prop_type in props.items():
             with self.subTest(prop=prop_name):
                 mock_parser.reset_mock()
-                field_name = prop_name.replace("_", "")
                 value = prop_type()
                 setattr(conf, prop_name, value)
 
                 mock_parser.set.assert_called_once_with(
-                    config.SETTINGS, field_name, str(value))
+                    config.SETTINGS, prop_name, str(value))
+
+    def testEmptyAlsaDevice(self):
+        config_path = pathlib.PurePath("/fake_dir/fake_path.txt")
+        conf = config.Config("Linux", config_path)
+        mock_parser = configparser.ConfigParser.return_value
+        none_str = "None"
+
+        conf.alsa_device = None
+        mock_parser.set.assert_called_once_with(
+            config.SETTINGS, config.ALSA_DEVICE, none_str)
+
+        mock_parser.get.return_value = none_str
+        self.assertIsNone(conf.alsa_device)
+
+        mock_parser.get.return_value = "some value"
+        self.assertEqual("some value", conf.alsa_device)
